c77f9a8c99c50b9821d9321a6dd1d397b0947771
Max Micciullo <mmicciullo@gmail.com>
2014-08-24 13:56:56 -0300

    Adding contact model and connection through jackson api to obtain json.

--- src/main/java/com/sample/controller/ContactController.java
+++ src/main/java/com/sample/controller/ContactController.java
@@ -1,10 +1,15 @@
 package com.sample.controller;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +19,16 @@ import org.springframework.web.servlet.ModelAndView;
 public class ContactController {
 	
 	@RequestMapping(value = "/contacts", method = RequestMethod.GET)
-	public ModelAndView contacts(HttpServletRequest request, HttpServletResponse response, Map<String, Object> model) {
+	public ModelAndView contacts(HttpServletRequest request, HttpServletResponse response, Map<String, Object> model) throws JsonParseException, JsonMappingException, MalformedURLException, IOException {
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+//		List<Contact> conacts = mapper.readValue(new URL("https://solstice.applauncher.com/external/contacts.json"), Contact.class);
+//		Map<String,Object> map = mapper.readValue(new URL("https://solstice.applauncher.com/external/contacts.json"), Map.class);		
+		
+		// just for test the response.
+//		System.out.println("Json: " + map);
+		
 		return new ModelAndView("contacts");
 	}
 }
---
+++ src/main/java/com/sample/model/Contact.java
@@ -0,0 +1,60 @@
+package com.sample.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Contact {
+	
+	private String name;
+	private Integer employeeId;
+	private String company;
+	private String detailsURL;
+	private String smallImageURL;
+	private Date birthdate;
+	private List<String> phone;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getDetailsURL() {
+		return detailsURL;
+	}
+	public void setDetailsURL(String detailsURL) {
+		this.detailsURL = detailsURL;
+	}
+	public String getSmallImageURL() {
+		return smallImageURL;
+	}
+	public void setSmallImageURL(String smallImageURL) {
+		this.smallImageURL = smallImageURL;
+	}
+	public Date getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+	public List<String> getPhone() {
+		return phone;
+	}
+	public void setPhone(List<String> phone) {
+		this.phone = phone;
+	}
+	
+}
